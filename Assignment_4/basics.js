2e6b551aa6aead3bd7d85a3c5d8614fa5d21bdfc
lasya2021 <167187276+lasya2021@users.noreply.github.com>
2024-05-05 08:42:27 +0100

    Assignments

---
+++ Assignment_4/basics.js
@@ -0,0 +1,21 @@
+var a=20
+var b=10
+var c= "-"
+
+
+if (c == "+")
+    {
+    console.log(a*b);
+}
+else if(c == "*")
+{
+    console.log(a*b);
+}
+else if(c == "-")
+{
+    console.log(a-b);
+}
+else if(c == "/")
+{
+    console.log(a/b);
+}
